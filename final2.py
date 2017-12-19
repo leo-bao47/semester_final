509454b28e06d66c0980c5bf1633972fbf5f68fc
Leo Bao <baolk@s.dcsdk12.org>
2017-12-18 23:27:00 -0700

    created functions for lists

--- final2.py
+++ final2.py
@@ -3,36 +3,40 @@ import sys
 data = open("bikes.csv", "r")
 lines = data.readlines()
 
-list_of_bikes = []
+list_of_travel = []
+list_of_wheel = []
+list_of_price = []
 
-def list_travel(list_of_bikes, travel):
+def list_travel(list_of_travel, travel):
 	for i in range(1, len(lines)):
 		info = lines[i].rstrip().split(",")
 
 		if info[5] == travel:
-			list_of_bikes.append(info[0])
-			list_of_bikes.append(info[1])
+			list_of_travel.append(info[1])
 
-	return list_of_bikes
+	return list_of_travel
 
-print list_travel(list_of_bikes, "90")
+print list_travel(list_of_travel, "200")
 
-def list_wheel(list_of_bikes, wheel):
+def list_wheel(list_of_wheel, wheel):
 	for i in range(1, len(lines)):
 		info = lines[i].rstrip().split(",")
 
 		if info[3] == wheel:
-			list_of_bikes.append(info[0])
-			list_of_bikes.append(info[1])
+			list_of_wheel.append(info[1])
 
-	return list_of_bikes
+	return list_of_wheel
 
-def list_price_range(list_of_bikes, price):
+print list_wheel(list_of_wheel, "29")
+
+def list_price_range(list_of_price, price):
 	for i in range(1, len(lines)):
 		info = lines[i].rstrip().split(",")
 
-		if info[4] == (price +- 500):
-			list_of_bikes.append(info[0])
-			list_of_bikes.append(info[1])
+		if info[4] == price:
+			list_of_price.append(info[1])
 
-	return list_of_bikes
+	return list_of_price
+
+print list_price_range(list_of_price, "4000")
+
