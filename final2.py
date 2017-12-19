9d3a47aa70a3ca288c8f4bc7869447067efd3010
Leo Bao <baolk@s.dcsdk12.org>
2017-12-18 22:35:14 -0700

    updated bikes.csv

--- final2.py
+++ final2.py
@@ -9,21 +9,21 @@ def list_travel(list_of_bikes, travel):
 	for i in range(1, len(lines)):
 		info = lines[i].rstrip().split(",")
 
-		if info[6] == (travel +- 10):
+		if info[5] == travel:
+			list_of_bikes.append(info[0])
 			list_of_bikes.append(info[1])
-			list_of_bikes.append(info[2])
 
 	return list_of_bikes
 
-print list_travel(list_of_bikes, "100")
+print list_travel(list_of_bikes, "90")
 
 def list_wheel(list_of_bikes, wheel):
 	for i in range(1, len(lines)):
 		info = lines[i].rstrip().split(",")
 
 		if info[3] == wheel:
+			list_of_bikes.append(info[0])
 			list_of_bikes.append(info[1])
-			list_of_bikes.append(info[2])
 
 	return list_of_bikes
 
@@ -32,7 +32,7 @@ def list_price_range(list_of_bikes, price):
 		info = lines[i].rstrip().split(",")
 
 		if info[4] == (price +- 500):
+			list_of_bikes.append(info[0])
 			list_of_bikes.append(info[1])
-			list_of_bikes.append(info[2])
 
 	return list_of_bikes
