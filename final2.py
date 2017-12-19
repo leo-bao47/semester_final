28440e1401be606cda4243b7d32a7b7078306768
Leo Bao <baolk@s.dcsdk12.org>
2017-12-18 23:56:53 -0700

    trying to compare lists

--- final2.py
+++ final2.py
@@ -16,8 +16,6 @@ def list_travel(list_of_travel, travel):
 
 	return list_of_travel
 
-print list_travel(list_of_travel, "200")
-
 def list_wheel(list_of_wheel, wheel):
 	for i in range(1, len(lines)):
 		info = lines[i].rstrip().split(",")
@@ -27,7 +25,6 @@ def list_wheel(list_of_wheel, wheel):
 
 	return list_of_wheel
 
-print list_wheel(list_of_wheel, "29")
 
 def list_price_range(list_of_price, price):
 	for i in range(1, len(lines)):
@@ -38,5 +35,34 @@ def list_price_range(list_of_price, price):
 
 	return list_of_price
 
-print list_price_range(list_of_price, "4000")
+list_price_range(list_of_price, "3800")
+list_wheel(list_of_wheel, "29")
+list_travel(list_of_travel, "90")
 
+def compare_lists():
+	price_travel_compare = []
+	for i in list_of_price:
+		if list_of_price(i) == list_of_travel(i):
+			price_travel_compare += i
+
+	final_list = []
+	for i in price_travel_compare:
+		if list_of_wheel(i) == price_travel_compare(i):
+			final_list += i
+
+	return final_list
+
+print compare_lists()
+
+"""def bike_finder(list_price_range, list_wheel, list_travel, price, travel, wheel):
+	print "These are the bikes that match your price range:"
+	print list_price_range(list_of_price, price)
+	print "------"
+	print "These are the bikes that match your travel requirements:"
+	print list_travel(list_of_travel, travel)
+	print "------"
+	print "These are the bikes that match your desired wheel size:"
+	print list_wheel(list_of_wheel, wheel)
+	print "------"
+
+bike_finder(list_price_range, list_wheel, list_travel, "3800", "90", "29")"""
--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print "3500 + 500"
+
+def compare_lists(list_price_range, list_wheel, list_travel, price, travel, wheel):
+	price_travel = []
+	for i in list_price_range(list_of_price, price), list_travel(list_of_travel, travel):
+		if list_price_range(list_of_price, price)[i] == list_travel(list_of_travel, travel)[i]:
+			price_travel += i
+
+	final_list = []
+	for i in price_travel, list_wheel(list_of_wheel, wheel):
+		if price_travel[i] == list_wheel(list_of_wheel, wheel)[i]:
+			final_list += i
+
+	return final_list
+
+print compare_lists(list_price_range, list_wheel, list_travel, "3800", "90", "29")
+
+
+
